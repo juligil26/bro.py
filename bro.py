f24b81ce295d69c296cf2d5c038f1a3121c5b35a
julian <julis14189@gmail.com>
2023-08-25 11:22:57 -0500

    NT4 se crearron los peluches

---
+++ bro.py
@@ -0,0 +1,28 @@
+peluches=["rochi","conejito","aguagatico"]
+
+opcion=0
+print("Pelucheria PELUCHITOS")
+print("....................")
+print("1. Agregar producto ala bodega")
+print("2. Ver productos en bodega")
+print("3. Obtener valor del inventario")
+print("4. Ver productos más vendidos")
+print("5.SALIR") 
+
+
+while opcion!=5:
+    opcion=int(input("Digita un numero: ")) 
+
+    if opcion==1:
+        print("bro estas en la opcion 1")
+    elif opcion==2:
+        print("bro estas en la opcion 2")
+    elif opcion==3:
+        print("bro estas en la opcion 3")
+    elif opcion==4:
+        print("bro estas en la opcion 4")
+    elif opcion==5:
+        print("bro chao pues")
+    else:
+        print("opcion invalida")
+print("sali del ciclo") 
